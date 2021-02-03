39da0eb4e5eb7ea2bd8d5a63e19d72a7c732772a
jamir ali <jamirali720@gmail.com>
2021-02-03 09:50:08 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,39 @@
+// undefined  when variable value is empty; 
+// const pakhi = ;
+// console.log(pakhi);
+// undefined when return is not done from function ;
+// function add (num1, num2){
+//     console.log(num1 + num2);
+//     // here is not return;
+// }
+// const result = add(20, 10);
+// console.log(result);
+
+
+
+// function add (num1, num2){
+//     console.log(num1 + num2);
+    
+// }
+// const result1 = add(50, 20);
+// console.log(result1);
+// // undefined when return has no value
+// function add (num1, num2){
+//     // console.log(num1 + num2);
+//     const sum = num1 + num2;
+    
+//     return;
+// }
+// const result2 = add(50, 20);
+// console.log(result2);
+
+// function add (num1, num2){
+//     console.log(num1, num2)
+
+// }
+// const result3 = add (12)
+// console.log(result3)
+const num = null;
+console.log(num)
+const ages = [2, 4, 6,  6,  9, 8,11];
+console.log(ages[7]);
