d66472057b32d24385b64061105036ab374afdf5
jamir ali <jamirali720@gmail.com>
2021-02-03 07:11:39 +0600

    truthy-falsy

---
+++ advance.js
@@ -0,0 +1,27 @@
+// const age = 0;
+// if(age ){
+//     console.log('condition is true');
+// }
+// else{
+//     console.log ('condition is false');
+// }
+
+
+// false shows when variable  is  NaN, Empty String "", 0, Undefine, null
+// truth shows when variable is empty array [], empty object{}, "0",  "  ",
+
+
+// const name = " ";
+// if(name.length ){
+//     console.log('condition is true');
+// }
+// else{
+//     console.log ('condition is false');
+// }
+const name = 12;
+if (name || name == 0) {
+    console.log('condition is true')
+
+}else{
+    console.log('condition is false')
+}
